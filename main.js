6b5efb4fff430254be21d66ec6264ff4cdd38024
jmclella <jmclella@fandm.edu>
2022-11-20 18:17:25 -0500

    Added comments to define functions

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 const gridContainer = document.querySelector('.grid-container');
 
+// Set rows within grid
 let setRows = 16;
 
+// Function creates 16 rows, 16 columns and appends divs within container
 for (let i = 0; i < setRows; i++) {
     const row = document.createElement('div');
     row.classList.add('container');
@@ -19,14 +21,17 @@ for (let i = 0; i < setRows; i++) {
 const cells = document.querySelectorAll('.cell');
 
 
+// Change color of cells when hovering
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
-        cell.setAttribute('style', 'background-color: #f4a688');
+        // Set color of cell when mouseover function is called
+        cell.setAttribute('style', 'background-color: #622569');
     })
 })
 
 const resetButton = document.querySelector('.resetButton');
 
+// Reset cell colors after reset button is clicked
 resetButton.addEventListener('click', () => {
     cells.forEach((cell) => {
         cell.setAttribute('style', 'background-color: #b7d7e8');
